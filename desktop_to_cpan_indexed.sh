80f19656bb08463c98cf05f35d0e2f602fe87518
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-08-05 18:10:38 -0700

    Update CPAN indexed Libmarpa API

---
+++ desktop_to_cpan_indexed.sh
@@ -0,0 +1,8 @@
+#!sh
+set -x
+test -d $HOME/Desktop/stage/Marpa--R2/r2/libmarpa/dev || exit 1
+(cd $HOME/Desktop/stage/Marpa--R2/r2/libmarpa/dev && make ../doc/api.html/index.html)
+test -d libmarpa_api/cpan_indexed || exit 1
+test -d $HOME/Desktop/stage/Marpa--R2/r2/libmarpa/doc/api.html || exit 1
+(cd libmarpa_api/cpan_indexed && rm *.html)
+(cd libmarpa_api/cpan_indexed && perl ../../api_doc_here.pl $HOME/Desktop/stage/Marpa--R2/r2/libmarpa/doc/api.html)

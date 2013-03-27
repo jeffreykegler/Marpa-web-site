add6ac4549a9e4a7eab35b27f9c69af267a073c7
Jeffrey Kegler <jkegl@cpan.org>
2013-03-26 21:18:22 -0700

    Remove Marpa::R3 script

--- r3_to_working.sh
+++
@@ -1,8 +0,0 @@
-#!sh
-set -x
-test -d ../r3/cpan/libmarpa/dev || exit 1
-(cd ../r3/cpan/libmarpa/dev && make ../doc/api.html/index.html)
-test -d libmarpa_api/latest || exit 1
-test -d ../r3/cpan/libmarpa/doc/api.html || exit 1
-(cd libmarpa_api/latest && rm *.html)
-(cd libmarpa_api/latest && perl ../../api_doc_here.pl ../../../r3/cpan/libmarpa/doc/api.html)

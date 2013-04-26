4cbbef482de49f3a2956e0941c2c3a48082f6566
Jeffrey Kegler <jkegl@cpan.org>
2013-04-26 09:54:09 -0700

    Update utilities

--- r2_to_working.sh
+++ r2_to_working.sh
@@ -1,8 +1,8 @@
 #!sh
 set -x
-test -d ../r2/r2/libmarpa/dev || exit 1
-(cd ../r2/r2/libmarpa/dev && make ../doc/api.html/index.html)
+test -d ../r2/cpan/libmarpa/dev || exit 1
+(cd ../r2/cpan/libmarpa/dev && make ../doc/api.html/index.html)
 test -d libmarpa_api/latest || exit 1
-test -d ../r2/r2/libmarpa/doc/api.html || exit 1
+test -d ../r2/cpan/libmarpa/doc/api.html || exit 1
 (cd libmarpa_api/latest && rm *.html)
-(cd libmarpa_api/latest && perl ../../api_doc_here.pl ../../../r2/r2/libmarpa/doc/api.html)
+(cd libmarpa_api/latest && perl ../../api_doc_here.pl ../../../r2/cpan/libmarpa/doc/api.html)

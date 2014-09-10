1bb014e107c858d191c05ce1a181e11c0e548ed5
Jeffrey Kegler <JKEGL@cpan.org>
2014-09-09 20:08:59 -0700

    Work on utilities

--- cp_cpandev.sh
+++ cp_cpandev.sh
@@ -3,11 +3,12 @@ set -x
 work_d=$HOME/stage/libmarpa.d/cpandev.d/work
 dev_d="$work_d"/dev
 doc_d="$work_d"/doc
+target_d=libmarpa_api/cpan_developer
 test -d "$dev_d" || exit 1
 (cd "$dev_d" && make ../doc/api.html/index.html)
 test -d libmarpa_api || exit 1
-rm -rf libmarpa_api/cpan_indexed
-mkdir libmarpa_api/cpan_indexed
+rm -rf "$target_d"
+mkdir "$target_d"
 test -d "$doc_d" || exit 1
-(cd libmarpa_api/cpan_indexed && rm *.html)
-(cd libmarpa_api/cpan_indexed && perl ../../api_doc_here.pl "$doc_d/api.html")
+(cd "$target_d" && rm *.html)
+(cd "$target_d" && perl ../../api_doc_here.pl "$doc_d/api.html")

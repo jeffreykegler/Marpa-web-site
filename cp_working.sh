475f1c5a0d652a4c8cf65dbb037997df2ce6a2b0
Jeffrey Kegler <JKEGL@cpan.org>
2014-09-09 20:14:24 -0700

    Work on utilities

--- cp_working.sh
+++ cp_working.sh
@@ -1,10 +1,14 @@
 #!sh
 set -x
-test -d ../r2/cpan/libmarpa/dev || exit 1
-(cd ../r2/cpan/libmarpa/dev && make ../doc/api.html/index.html)
+work_d=$HOME/stage/libmarpa.d/master.d/work
+dev_d="$work_d"/dev
+doc_d="$work_d"/doc
+target_d=libmarpa_api/latest
+test -d "$dev_d" || exit 1
+(cd "$dev_d" && make ../doc/api.html/index.html)
 test -d libmarpa_api || exit 1
-rm -rf libmarpa_api/latest
-mkdir libmarpa_api/latest
-test -d ../r2/cpan/libmarpa/doc/api.html || exit 1
-(cd libmarpa_api/latest && rm *.html)
-(cd libmarpa_api/latest && perl ../../api_doc_here.pl ../../../r2/cpan/libmarpa/doc/api.html)
+rm -rf "$target_d"
+mkdir "$target_d"
+test -d "$doc_d" || exit 1
+(cd "$target_d" && rm *.html)
+(cd "$target_d" && perl ../../api_doc_here.pl "$doc_d/api.html")

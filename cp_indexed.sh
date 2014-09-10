2b1cc82c7a41a2f905d046193b325afa812824d8
Jeffrey Kegler <JKEGL@cpan.org>
2014-09-09 19:40:25 -0700

    Work on utilities

--- cp_indexed.sh
+++ cp_indexed.sh
@@ -1,10 +1,13 @@
 #!sh
 set -x
-test -d $HOME/Desktop/stage/Marpa--R2/cpan/libmarpa/dev || exit 1
-(cd $HOME/Desktop/stage/Marpa--R2/cpan/libmarpa/dev && make ../doc/api.html/index.html)
+work_d=$HOME/stage/libmarpa.d/indexed.d/work
+dev_d="$work_d"/dev
+doc_d="$work_d"/doc
+test -d "$dev_d" || exit 1
+(cd "$dev_d" && make ../doc/api.html/index.html)
 test -d libmarpa_api || exit 1
 rm -rf libmarpa_api/cpan_indexed
 mkdir libmarpa_api/cpan_indexed
-test -d $HOME/Desktop/stage/Marpa--R2/cpan/libmarpa/doc/api.html || exit 1
+test -d "$doc_d" || exit 1
 (cd libmarpa_api/cpan_indexed && rm *.html)
-(cd libmarpa_api/cpan_indexed && perl ../../api_doc_here.pl $HOME/Desktop/stage/Marpa--R2/cpan/libmarpa/doc/api.html)
+(cd libmarpa_api/cpan_indexed && perl ../../api_doc_here.pl "$doc_d/api.html")

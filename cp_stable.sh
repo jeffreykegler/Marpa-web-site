d28f97d908498d2146addfa3964702a918bedd3b
Jeffrey Kegler <JKEGL@cpan.org>
2015-02-09 20:15:39 -0800

    Update utilities

--- cp_stable.sh
+++ cp_stable.sh
@@ -1,11 +1,28 @@
 #!sh
 set -x
-source_d=$HOME/stage/libmarpa.d/master.d
+
+# NOTE: This is NOT portable
+# It relies on my directory layout
+libmarpa_dir=$HOME/projects/libmarpa
+
+if test -d $libmarpa_dir
+then :
+else
+    echo $libmarpa_dir not found 1>&2
+    exit 1
+fi
+branch=stage
+source_d=stage/$branch.d
 work_d=$source_d/work
 dev_d="$work_d"/dev
 doc_d="$work_d"/doc
 doc1_d="$work_d"/doc1
 target_d=libmarpa_api/stable
+
+test -d stage || mkdir stage
+rm -rf $source_d
+git clone -b "$branch" file://$libmarpa_dir "$source_d"
+
 test -d "$dev_d" || exit 1
 (cd "$dev_d" && make ../doc/api.html/index.html)
 test -d libmarpa_api || exit 1
@@ -17,6 +34,6 @@ test -d "$doc_d" || exit 1
 test -d "$doc1_d" || exit 1
 (cd $doc1_d && ./configure && make html)
 (cd "$target_d" && rm *.html)
-(cd "$target_d" && perl ../../api_doc_here.pl "$doc_d/api.html")
+(cd "$target_d" && perl ../../api_doc_here.pl "../../$doc_d/api.html")
 cp "$doc1_d/api.html" temp/api_one_page.html
 (cd "$target_d" && perl ../../api_doc_here.pl ../../temp/api_one_page.html)
